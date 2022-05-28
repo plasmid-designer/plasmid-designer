444e298811a764a1be1be8717c4d1167295edbd1
Marco Quinten <splittydev@gmail.com>
2022-05-28 02:00:20 +0200

    Fix possible bug in `withTimer`

--- src/util/withTimer.js
+++ src/util/withTimer.js
@@ -1,10 +1,6 @@
 export const withTimer = name => fn => (...params) => {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        const start = Date.now()
-        const result = fn(...params)
-        console.log(`[${name}] ${Date.now() - start}ms`)
-        return result
-    } else {
-        return fn(...params)
-    }
+    const start = Date.now()
+    const result = fn(...params)
+    console.log(`[${name}] ${Date.now() - start}ms`)
+    return result
 }
